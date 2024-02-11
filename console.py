2f4d519e0242de88a2a2dacecbe648d4249df75a
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-02-11 15:55:56 +0100

    console

--- console.py
+++ console.py
@@ -6,9 +6,11 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
     """ Console desplay in python along with its method"""
     def do_EOF(self, line):
-        """Exit"""
+        """EOF command to exit the program"""
         return True
 
     def do_quit(self, line):
