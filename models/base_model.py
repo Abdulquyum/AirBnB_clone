523a8e196f4ad3b071fcdabfdbd7086163cba733
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-02-12 22:23:01 +0100

    AirBnB

--- models/base_model.py
+++ models/base_model.py
@@ -13,25 +13,22 @@ class BaseModel():
     perfect execution of the project """
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        date_now = datetime.now()
         date_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.created_at = date_now.strftime(date_format)
-        self.updated_at = date_now.strftime(date_format)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs):
             for key, value in kwargs.items():
-                if key in ["creted_at", "updated_at"]:
+                if key in ["created_at", "updated_at"]:
                     self.__dict__[key] = datetime.strptime(value, date_format)
                 else:
                     self.__dict__[key] = value
-        # else:
-            # self.models.new(self)
 
     def __str__(self):
         return f"{[__class__.__name__]} {(self.id)} {self.__dict__}"
 
     def save(self):
-        date_now = datetime.now()
-        self.updated_at = datetime.strftime(date_now, "%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
@@ -39,8 +36,3 @@ class BaseModel():
         obj_dict['updated_at'] = self.updated_at.isoformat()
         obj_dict['__class__'] = self.__class__.__name__
         return obj_dict
-
-    def save(self):
-        date_now = datetime.now()
-        self.updated_at = datetime.strftime(date_now, "%Y-%m-%dT%H:%M:%S.%f")
-        storage.save
