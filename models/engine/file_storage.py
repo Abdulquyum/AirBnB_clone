34d696a54a5829fd2aa352ed41d9890b7537f7b4
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-02-13 18:55:31 +0100

    AirBnB

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,9 +20,6 @@ class FileStorage():
         self.__objects = obj.id
 
     def save(self):
-        # with open(FileStorage.__file_path, 'w') as f:
-        # json.dumps(f)
-
         dup_obj = FileStorage.__objects
         obj_dict = {obj: dup_obj[obj].to_dict() for obj in dup_obj.keys()}
 
